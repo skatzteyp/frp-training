b9440285e366bb6510d6773455792af31c3c8572
Sigmund Burgos <sigmund_burgos@ligph.com>
2018-06-12 09:38:43 +0800

    Added babel loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './public/scripts.js',
+  entry: './src/scripts.js',
   output: {
     filename: 'scripts.js',
     path: path.resolve(__dirname, 'public')
-  }
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /(node_modules|bower_components)/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['stage-2']
+  //         }
+  //       }
+  //     }
+  //   ]
+  // }
 }
